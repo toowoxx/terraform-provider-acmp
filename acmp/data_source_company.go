4093b6f7244ef55a66247a906c531fa198418ced
Patrick Bossier <patrick.bossier@toowoxx.de>
2021-07-13 11:36:58 +0200

    added provider files

---
+++ acmp/data_source_company.go
@@ -0,0 +1,15 @@
+package acmp
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+  )
+  
+func
--- main.go
+++ main.go
@@ -1,14 +1,15 @@
 package main
 
 import (
-  "github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-  "github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
+	"terraform-provider/acmp"
 )
 
 func main() {
-  plugin.Serve(&plugin.ServeOpts{
-    ProviderFunc: func() *schema.Provider {
-      return also-cloud-marketplace.Provider()
-    },
-  })
+	plugin.Serve(&plugin.ServeOpts{
+		ProviderFunc: func() *schema.Provider {
+			return acmp.Provider()
+		},
+	})
 }
